9174fb0cac64d6b774bfaaa0d4d3b2deb152c7aa
FatManDJ <qw200237ml@outlook.com>
2020-07-26 17:12:44 +0800

    #2 🌮 Update

--- page/friends.md
+++
@@ -1,3 +0,0 @@
-# 友链
-
-XHG78999：[XHG78999](https://blog-xhg78999.cnblog.com)
--- read.py
+++
@@ -1,9 +0,0 @@
-import yaml
-def get_yaml_data(yaml_file):
-    # 打开yaml文件
-# 直接打开读出来
-    f = open(yaml_file,'r',encoding='utf-8')
-    result = f.read()
-    # 转换成字典读出来
-    a = yaml.load(result,Loader=yaml.FullLoader)
-    return a
